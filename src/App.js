82aa1eebed964825784deb8f7518b1c5c42aa407
zzzmini <sm01079200887@gmail.com>
2025-01-17 10:05:13 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="black-bg">
+        React로 만드는 블로그
+      </div>
+    </div>
+  );
+}
+
+export default App;
